97856193d41b862f188082d5c0ca9a0ba7fd736a
Trường Nguyễn Nhật <6151071110@st.utc2.edu.vn>
2024-06-08 23:43:13 +0700

    upload source code

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config
+{
+	const TITLE = 'Hệ Thống Trắc Nghiệm Online';
+	const COPYRIGHT = "Copyright © 2024 By Truong";
+	const VERSION = "3.3.0";
+	const OWNER = "Nguyễn Nhật Trường";
+	const CONTRIBUTOR = "";
+	const EMAIL = "6151971110@st.utc2.edu.vn";
+	const RELEASE = "8/6/2024";
+	const TIMEZONE = "Asia/Bangkok";
+}
---
+++ config/connect.php
@@ -0,0 +1,3 @@
+<?php
+return (object) array('host' => 'localhost','user' => 'root','password' => '','dbname' => 'website_tracnghiem','INSTALL_MODE' => FALSE);
+?>
